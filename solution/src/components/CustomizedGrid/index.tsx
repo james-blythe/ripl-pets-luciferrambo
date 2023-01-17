0ff74fb69cb2a30871b1038af34264d0c758cd6a
Rumbo <engineerr0528@gmail.com>
2023-01-17 10:16:17 -0800

    Added a button component.

--- solution/src/components/CustomizedGrid/index.tsx
+++ solution/src/components/CustomizedGrid/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import { Col, Row } from 'antd';
+import { Button } from 'antd';
 import CustomizedCard from '../CustomizedCard';
 
 const App: React.FC = () => (
   <>
+    <Button
+      style={{
+        backgroundColor: '#1677ff',
+        color: 'white',
+        fontSize: '30px',
+        width: '200px',
+        height: '50px',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+    >
+      Featured Pets
+    </Button>
+    <Button>Filter</Button>
     <Row>
       <Col xs={{ span: 7, offset: 1 }} lg={{ span: 6, offset: 2 }}>
         <CustomizedCard />
